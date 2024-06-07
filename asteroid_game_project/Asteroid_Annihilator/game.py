8faf9b40240336edc539d5d73bef2881b9860ed0
Rhys Yearwood <ryearwood1@live.com>
2024-06-07 18:01:22 +0000

    moved readme

--- asteroid_game_project/Asteroid_Annihilator/game.py
+++ asteroid_game_project/Asteroid_Annihilator/game.py
@@ -1,24 +1,12 @@
-# imports pygame module
-
 import pygame
-
-# creates the AsteroidAnnihilator class
 
 class AsteroidAnnihilator:
 
-    # the constructor
-
     def __init__(self):
-
-        # for pygame initialization
 
         self._init_pygame()
 
-        # creates a display surface of 800 pixels width and 600 pixels height
-
         self.screen = pygame.display.set_mode((800, 600))
-
-    # main loop for every frame
     
     def main_loop(self):
         while True:
@@ -26,15 +14,9 @@ class AsteroidAnnihilator:
             self._process_game_logic()
             self._draw()
 
-    # one-time initialization of pygame
-
     def _init_pygame(self):
 
-        # calls pygame.init for its feautures
-
         pygame.init()
-
-        #sets the caption of this Pygame programme
 
         pygame.display.set_caption("Asteroid Annihilator")
 
